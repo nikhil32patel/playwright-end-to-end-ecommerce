2a6fc8a43bf88ebe7fbb6042b32e350fefbfef58
nikhil32patel <nikhil32patel@gmail.com>
2026-01-11 13:57:55 +0530

    e2e project set up

---
+++ test.config.ts
@@ -0,0 +1,17 @@
+export class TestConfig{
+ 
+    //appUrl="http://localhost/opencart/upload/"
+    //appUrl="https://naveenautomationlabs.com/opencart"
+    appUrl="https://tutorialsninja.com/demo/"
+    
+    
+  
+    //valid login credentials
+    email="nbtest@gmail.com"
+    password="Nbtest@123"
+
+    //product details
+    productName="MacBook"
+    productQuantity="2"
+    totalPrice="$1,204.00"
+}
